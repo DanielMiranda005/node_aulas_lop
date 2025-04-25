eb104df3a09ec272b4c2f9d8c2fd6166c2c5e924
DANIEL FORTUNATO MIRANDA <25170152@educ123.sp.senai.br>
2025-04-25 07:53:23 -0300

    adicionando codigo moldura

--- funções/moldura.js
+++ funções/moldura.js
@@ -1,1 +1,19 @@
 //Desenha uma moldura mem uma frase
+
+
+function barra(qtde){
+   for (let i=0; i<qtde+4; i++) {
+      process.stdout.write(ch);
+  
+   }
+}
+
+ const readline = require('readline-sync');
+ const frase = readline.question('\nFrase: ');
+ const ch = '█';
+
+
+barra(frase.length)
+console.log(`\n${ch} ${frase} ${ch}`);
+barra(frase.length)
+
